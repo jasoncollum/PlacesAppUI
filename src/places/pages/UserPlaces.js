289e07387b636da7d3096c362204b764652c27ae
Jason Collum <jasoncollum@gmail.com>
2020-02-27 14:08:36 -0600

    setup UserPlaces route

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
+import PlaceList from '..//components/PlaceList';
+
+const PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous skyscapers in the world!',
+        imageURL: 'http://upload.wikimedia.org/wikipedia/commons/d/df/NYC_Empire_State_Building.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        description: 'One of the most famous skyscapers in the world!',
+        imageURL: 'http://upload.wikimedia.org/wikipedia/commons/d/df/NYC_Empire_State_Building.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u2'
+    }
+];
+
 const UserPlaces = () => {
-    return
+    return <PlaceList items={PLACES} />
 };
 
 export default UserPlaces;
