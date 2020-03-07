21a7920f7426cfc4a9af83f173c14e5924be2ab5
Jason Collum <jasoncollum@gmail.com>
2020-03-07 13:03:02 -0600

    add fetch call to get users, add isLoading, error, userData useState hooks

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS = [
-        {
-            id: 'u1',
-            name: 'Jason Collum',
-            image: 'https://www.jasoncollum.com/img/jason.png',
-            places: 3
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [usersData, setUsersData] = useState();
+
+    useEffect(() => {
+        const sendRequest = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch('http://localhost:5000/api/users');
+                const responseData = await response.json();
+
+                if (!response.ok) {
+                    throw new Error(responseData.message)
+                }
+
+                setUsersData(responseData.users);
+            } catch (err) {
+                setError(err.message);
+            }
+            setIsLoading(false);
         }
-    ];
+        sendRequest();
+    }, []);
+
+    const errorHandler = () => {
+        setError(null);
+    };
+
     return <UsersList items={USERS} />;
 }
 
